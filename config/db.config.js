07f3a756712a098711156a9c626877a06766d42e
sayyedaaman2 <sayyedaaman9@gmail.com>
2026-02-17 19:08:56 +0530

    Optimized Database Operations

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 
 const databaseConfig = {
-    DATABASE_FILE_NAME : process.env.DATABASE_FILE_NAME
+    DATABASE_FILE_NAME : process.env.DATABASE_FILE_NAME,
+    DATABASE_FILE_PATH : process.env.DATABASE_FILE_PATH
 }
 
 export default databaseConfig;
